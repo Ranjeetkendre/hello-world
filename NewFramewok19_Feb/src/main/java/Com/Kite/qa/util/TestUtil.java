c2abf7db48ee2ffde236fa0705b36feecbe76856
Ranjeet <kendreranjet07@gmail.com>
2022-06-05 21:30:35 +0530

    Created new framework

---
+++ NewFramewok19_Feb/src/main/java/Com/Kite/qa/util/TestUtil.java
@@ -0,0 +1,9 @@
+package Com.Kite.qa.util;
+
+public class TestUtil {
+
+	//public static long PAGE_LOAD_TIMEOUT =20;
+	public static long IMPLICIT_WAIT =10;
+	public static long PAGE_LOAD_TIMEOUT=20;
+}
+
---
+++ NewFramewok19_Feb/src/main/java/resources/ExtentReporterNG.java
@@ -0,0 +1,28 @@
+package resources;
+
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.reporter.ExtentSparkReporter;
+
+public class ExtentReporterNG {
+	static ExtentReports extent;
+
+	public static ExtentReports getReportObject()
+	
+	{		
+		String path =System.getProperty("user.dir")+"\\reports\\index.html";
+		
+		ExtentSparkReporter reporter = new ExtentSparkReporter(path);
+		
+		reporter.config().setReportName("Kite application Report");
+		
+		reporter.config().setDocumentTitle("Test Results");
+		
+		extent =new ExtentReports();
+		
+		extent.attachReporter(reporter);
+		
+		extent.setSystemInfo("API Automation Engineer ", "Ranjeet Kendre");
+		
+		return extent;	
+	}
+}
